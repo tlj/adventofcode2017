7c16c539ad9bfe87aeb9ffacea391c48ddac09a1
tlj <tlj@tlj.no>
2017-12-01 20:56:41 +0100

    Day01

---
+++ AdventOfCode/Input.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AdventOfCode
+{
+    public class Input
+    {
+        public static List<char> StringToList(string input)
+        {            
+            var inputArr = new List<char>();
+            inputArr.AddRange(input);
+            return inputArr;
+        }
+    }
+}

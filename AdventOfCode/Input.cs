30c9db5fd681eb7de4f737067e52a88e782df30a
tlj <tlj@tlj.no>
2017-12-02 09:13:25 +0100

    Day2

--- AdventOfCode/Input.cs
+++ AdventOfCode/Input.cs
@@ -11,5 +11,22 @@ namespace AdventOfCode
             inputArr.AddRange(input);
             return inputArr;
         }
+
+        public static List<List<int>> StringToListsOfInt(string input, string splitChar)
+        {
+            var lines = new List<List<int>>();
+            foreach (var line in input.Split(Environment.NewLine))
+            {
+                var l = new List<int>();
+                foreach (var c in line.Split(splitChar))
+                {
+                    l.Add(int.Parse(c));
+                }
+
+                lines.Add(l);
+            }
+
+            return lines;
+        }
     }
 }
---
+++ AdventOfCode2017/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace AdventOfCode2017
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Day02.Part1();
+            Day02.Part2();
+        }
+    }
+}
